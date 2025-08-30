7569867f4a236cd9d9f51fadc3238dce73b54a7a
OvOpenguin <rosebox14@gmail.com>
2025-08-30 21:32:00 +0800

    0830-home更新sticjy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,77 @@ const Home = () => {
                 </section>
 
                 {/* 事件資訊區: 視差滾動 */}
-                <section id="info"></section>
+                <section id="info">
+                    <article className="card">
+                        <div className="card-l"><h2>16</h2></div>
+                        <div className="card-r">
+                                {/* 年分+按鈕 */}
+                                <div> 
+                                    <h3>36</h3>
+                                    <img src="#" alt="回溯鍵" />
+                                </div>
+                                {/* 標題內文 */}
+                                <div className="content">
+                                    <h3>鬱金香狂熱</h3>
+                                    <p>歷史上最離奇的泡沫經濟，一朵鬱金香的價格竟然能買下一棟豪宅？
+                                        深入了解這場讓整個荷蘭為之瘋狂的「鬱金香狂熱」，看小小花卉如何引發一場席捲全國的金融風暴。</p>
+                                </div>
+                                {/* 附圖 */}
+                                <figure><img src="./info1.png" alt="圖" /></figure>
+
+                            </div>
+
+
+                    </article>
+                    <article className="card">
+                        <div className="card-l"><h2>16</h2></div>
+                        <div className="card-r">
+                                {/* 年分+按鈕 */}
+                                <div> 
+                                    <h3>36</h3>
+                                    <img src="#" alt="回溯鍵" />
+                                </div>
+                                {/* 標題內文 */}
+                                <div className="content">
+                                    <h3>鬱金香狂熱</h3>
+                                    <p>歷史上最離奇的泡沫經濟，一朵鬱金香的價格竟然能買下一棟豪宅？
+                                        深入了解這場讓整個荷蘭為之瘋狂的「鬱金香狂熱」，看小小花卉如何引發一場席捲全國的金融風暴。</p>
+                                </div>
+                                {/* 附圖 */}
+                                <figure><img src="./info1.png" alt="圖" /></figure>
+
+                            </div>
+
+
+                    </article>
+                    <article className="card">
+                        <div className="card-l"><h2>16</h2></div>
+                        <div className="card-r">
+                                {/* 年分+按鈕 */}
+                                <div> 
+                                    <h3>36</h3>
+                                    <img src="#" alt="回溯鍵" />
+                                </div>
+                                {/* 標題內文 */}
+                                <div className="content">
+                                    <h3>鬱金香狂熱</h3>
+                                    <p>歷史上最離奇的泡沫經濟，一朵鬱金香的價格竟然能買下一棟豪宅？
+                                        深入了解這場讓整個荷蘭為之瘋狂的「鬱金香狂熱」，看小小花卉如何引發一場席捲全國的金融風暴。</p>
+                                </div>
+                                {/* 附圖 */}
+                                <figure><img src="./info1.png" alt="圖" /></figure>
+
+                            </div>
+
+
+                    </article>
+                    
+                </section>
+
+
+
+
+
 
                 {/* 隨機選擇區 */}
                 <section id="random"></section>
