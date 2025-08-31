7fd6f18bd61a5052d3761fb88b5d80571f79b8ae
OvOpenguin <rosebox14@gmail.com>
2025-08-31 22:07:57 +0800

    0831_新增漢堡按鈕

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,36 @@
+import $ from 'jquery'
+import { useEffect } from 'react'
 import { Link } from "react-router-dom"
 
+
+
 const Nav = () => {
+
+    // // jquery 須放在useEffect內使用
+    useEffect(() => {
+        $(".hamburger").on("click", function () {
+            $(this).toggleClass("is-active");
+            $(".menu").toggleClass("show");
+        });
+
+        // 清除事件，避免重複綁定
+        return () => {
+            $(".hamburger").off("click");
+        };
+    }, []);
+
+
     return (
         <header>
             <nav>
-                <button className="btnPause">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 20 21" fill="none">
-                        <rect width="19.2325" height="19.2325" transform="translate(0.313568 0.980469)" fill="none" />
-                        <rect x="4.64087" y="3.13867" width="3.36568" height="15.386" rx="1.68284" fill="#323232" />
-                        <rect x="11.8531" y="3.13867" width="3.36568" height="15.386" rx="1.68284" fill="#323232" />
+                <button className="hamburger">
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="28" viewBox="0 0 16 28" fill="none">
+                        <path d="M15.1913 16.1673L2.46588 27.6519C1.38593 28.6255 0 27.4466 0 25.4837V2.51451C0 0.554783 1.38393 -0.627274 2.46588 0.349478L15.1913 11.8341C15.4369 12.0522 15.6411 12.3675 15.7832 12.748C15.9252 13.1285 16 13.5607 16 14.0007C16 14.4407 15.9252 14.8728 15.7832 15.2533C15.6411 15.6338 15.4369 15.9491 15.1913 16.1673V16.1673Z" fill="#323232" />
                     </svg>
                 </button>
-                <ul className="nav">
+                <ul className="menu">
                     <li><Link to="/">回到現在</Link></li>
                     <li><Link to="/travel">時空回溯</Link></li>
                     <li><Link to="/events">情報蒐集</Link></li>
