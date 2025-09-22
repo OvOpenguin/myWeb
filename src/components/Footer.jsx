6cdaa5333cdba19505e1430708cfe6265f5df678
OvOpenguin <rosebox14@gmail.com>
2025-09-22 22:28:30 +0800

    0922-更新佈署與字形

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer>copyright &copy; 2025 回溯時空 777號.</footer>
+        <footer>copyright &copy; 2025 回溯時空 <small>(學生練習作品)</small></footer>
     )
 }
 
